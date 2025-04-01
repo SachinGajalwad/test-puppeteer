5dae612c184385f493716ef7abb40b3057758572
Sachin <50242145+SachinGajalwad@users.noreply.github.com>
2025-04-01 22:57:36 +0530

    b

--- server/server.ts
+++ server/server.ts
@@ -23,4 +23,4 @@ app.get("/", async (req: any, res: any) => {
     }
 });
 
-app.listen(8080, () => console.log("Server running on port 8080"));
+app.listen(3000, () => console.log("Server running on port 3000"));
